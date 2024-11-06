680a0b1b8e0e2661dc07b47c1d448876ae0734bf
Jorge <jorgemf@gmail.com>
2024-11-06 01:07:21 +0100

    error added

--- src/OtherOwnableValidator2/OtherOwnableValidator2.sol
+++ src/OtherOwnableValidator2/OtherOwnableValidator2.sol
@@ -17,7 +17,7 @@ uint256 constant TYPE_STATELESS_VALIDATOR = 7;
  * @author Rhinestone
  */
 
-contract OwnableValidator2 is ERC7579ValidatorBase {
+contract OtherOwnableValidator2 is ERC7579ValidatorBase {
     using LibSort for *;
     using SentinelList4337Lib for SentinelList4337Lib.SentinelList;
 
